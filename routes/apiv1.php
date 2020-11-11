6d425bd5055e9a06ea14e84752f93c080039ce86
Victor Gutierrez <vgutierrezc.90@gmail.com>
2020-11-10 22:48:27 -0600

    :sparkles: configured api file

--- routes/apiv1.php
+++ routes/apiv1.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(Request $request) {
+    return response()->json(['test' => 'test call']);
 });
+// Route::group(['middleware' => ['auth:api']], function () {
+// });
